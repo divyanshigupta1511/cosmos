908914a1b028dbacdf21eacff3f05448228555e4
Divyanshi <divyanshigupta.1511@gmail.com>
2026-07-31 13:57:42 +0530

    Add Microsoft Clarity tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -188,13 +188,29 @@ className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
   "
 >
   <Script
-  id="structured-data"
-  type="application/ld+json"
-  strategy="beforeInteractive"
-  dangerouslySetInnerHTML={{
-    __html: JSON.stringify(structuredData),
-  }}
+  src="https://www.googletagmanager.com/gtag/js?id=G-0218LHQ8YT"
+  strategy="afterInteractive"
 />
+
+<Script id="google-analytics" strategy="afterInteractive">
+  {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-0218LHQ8YT');
+  `}
+</Script>
+<Script id="microsoft-clarity" strategy="afterInteractive">
+  {`
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "xuyk4n7sbp");
+  `}
+</Script>
+
   
   <PageLoader />
   <ScrollProgress />
--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -36,16 +36,16 @@ const handleSubmit = async (e: React.FormEvent) => {
 
  try {
   await emailjs.send(
-    "service_lm2t0cu",
-    "template_wfeaioh",
-    {
-      name: formData.name,
-      email: formData.email,
-      company: formData.company,
-      message: formData.message,
-    },
-    "qhZz4bM5sFlmwrTcu"
-  );
+  process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
+  process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
+  {
+    name: formData.name,
+    email: formData.email,
+    company: formData.company,
+    message: formData.message,
+  },
+  process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!
+);
 
   setSuccess("Message sent successfully!");
 
